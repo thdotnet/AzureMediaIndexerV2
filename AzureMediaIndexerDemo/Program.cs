fc162c42dd496e7772d097d1eb1b484c88875bcb
thiago custodio <thiago custodio>
2016-07-12 15:03:56 -0300

    initial commit

---
+++ AzureMediaIndexerDemo/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MediaServices.Client;
+using System.Configuration;
+using System.IO;
+using System.Threading;
+
+namespace AzureMediaIndexerDemo
+{
+    using System;
+    using System.Configuration;
+    using System.IO;
+    using System.Linq;
+    using Microsoft.WindowsAzure.MediaServices.Client;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    namespace IndexContent
+    {
+        class Program
+        {
+            private static readonly string _mediaServicesAccountName = ConfigurationManager.AppSettings["MediaServicesAccountName"];
+            private static readonly string _mediaServicesAccountKey = ConfigurationManager.AppSettings["MediaServicesAccountKey"];
+
+            private static CloudMediaContext _context = null;
+            private static MediaServicesCredentials _cachedCredentials = null;
+
+            static void Main(string[] args)
+            {
+                _cachedCredentials = new MediaServicesCredentials(_mediaServicesAccountName, _mediaServicesAccountKey);
+                _context = new CloudMediaContext(_cachedCredentials);
+
+                var videoFile = @"C:\Users\usuario\Desktop\teste\campanha.mp4";
+                var configFile = @"C:\Users\usuario\Desktop\teste\config.json";
+
+                var asset = RunIndexingJob(videoFile, configFile);
+
+                DownloadAsset(asset, @"C:\Users\usuario\Desktop\teste\Output");
+            }
+
+            static IAsset RunIndexingJob(string inputMediaFilePath, string configurationFile)
+            {
+                IAsset asset = CreateAssetAndUploadSingleFile(inputMediaFilePath, "My Indexing Input Asset", AssetCreationOptions.None);
+
+                IJob job = _context.Jobs.Create("My Indexing Job");
+                string MediaProcessorName = "Azure Media Indexer 2 Preview";
+
+                var processor = GetLatestMediaProcessorByName(MediaProcessorName);
+
+                string configuration = File.ReadAllText(configurationFile);
+
+                ITask task = job.Tasks.AddNew("My Indexing Task", processor, configuration, TaskOptions.None);
+
+                task.InputAssets.Add(asset);
+                task.OutputAssets.AddNew("My Indexing Output Asset", AssetCreationOptions.None);
+
+                job.StateChanged += new EventHandler<JobStateChangedEventArgs>(StateChanged);
+                job.Submit();
+
+                Task progressJobTask = job.GetExecutionProgressTask(CancellationToken.None);
+
+                progressJobTask.Wait();
+
+                if (job.State == JobState.Error)
+                {
+                    ErrorDetail error = job.Tasks.First().ErrorDetails.First();
+                    Console.WriteLine(string.Format("Error: {0}. {1}", error.Code, error.Message));
+                    return null;
+                }
+
+                return job.OutputMediaAssets[0];
+            }
+
+            static IAsset CreateAssetAndUploadSingleFile(string filePath, string assetName, AssetCreationOptions options)
+            {
+                IAsset asset = _context.Assets.Create(assetName, options);
+
+                var assetFile = asset.AssetFiles.Create(Path.GetFileName(filePath));
+                assetFile.Upload(filePath);
+
+                return asset;
+            }
+
+            static void DownloadAsset(IAsset asset, string outputDirectory)
+            {
+                foreach (IAssetFile file in asset.AssetFiles)
+                {
+                    file.Download(Path.Combine(outputDirectory, file.Name));
+                }
+            }
+
+            static IMediaProcessor GetLatestMediaProcessorByName(string mediaProcessorName)
+            {
+                var processor = _context.MediaProcessors
+                                        .Where(p => p.Name == mediaProcessorName)
+                                        .ToList()
+                                        .OrderBy(p => new Version(p.Version))
+                                        .LastOrDefault();
+
+                if (processor == null)
+                    throw new ArgumentException(string.Format("Unknown media processor", mediaProcessorName));
+
+                return processor;
+            }
+
+            static private void StateChanged(object sender, JobStateChangedEventArgs e)
+            {
+                Console.WriteLine("Job state changed event:");
+                Console.WriteLine("  Previous state: " + e.PreviousState);
+                Console.WriteLine("  Current state: " + e.CurrentState);
+
+                switch (e.CurrentState)
+                {
+                    case JobState.Finished:
+                        Console.WriteLine();
+                        Console.WriteLine("Job is finished.");
+                        Console.WriteLine();
+                        break;
+                    case JobState.Canceling:
+                    case JobState.Queued:
+                    case JobState.Scheduled:
+                    case JobState.Processing:
+                        Console.WriteLine("Please wait...\n");
+                        break;
+                    case JobState.Canceled:
+                    case JobState.Error:
+                        IJob job = (IJob)sender;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
